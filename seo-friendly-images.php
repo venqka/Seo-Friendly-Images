b5f646e51e9742f969c144f4ac7ec4fe6715a124
venqka <trezvenqka@abv.bg>
2018-03-06 05:41:49 +0200

    Add license details

--- seo-friendly-images.php
+++ seo-friendly-images.php
@@ -9,6 +9,21 @@
  * License: GPL2
  */
 
+/*
+SEO Friendly Images is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+SEO Friendly Images is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with Redirect transliterated. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+*/
+
 include ( plugin_dir_path( __FILE__ ) . 'sfi-settings.php' );
 include ( plugin_dir_path( __FILE__ ) . 'sfi-embeded-images.php' );
 include ( plugin_dir_path( __FILE__ ) . 'sfi-featured-images.php' );
