466ce71cede2254cc756cc58015461cc4bbdb05b
Venelin Borisov <venelinborisov5@gmail.com>
2020-01-06 07:20:00 +0200

    Remove WooCommerce galleries code

--- sfi-wc-featured-images.php
+++ sfi-wc-featured-images.php
@@ -10,9 +10,7 @@ class fsiWC {
 	public function __construct() {
 		
 		add_filter( 'wp_get_attachment_image_attributes', array( $this, 'sffi_wc_set_attributes' ), 11, 2 );
-
-		add_filter( 'woocommerce_single_product_image_thumbnail_html', array( $this, 'clear_product_thumb_attributes' ), 11 );
-				
+			
 	}
 
 	function sffi_wc_set_attributes( $attr ) {
@@ -100,30 +98,6 @@ class fsiWC {
 			
 		
 		return $attr;
-	}
-
-	function clear_product_thumb_attributes( $html ) {
-
-			global $post, $product;
-			
-			$columns           = apply_filters( 'woocommerce_product_thumbnails_columns', 4 );
-			$thumbnail_size    = apply_filters( 'woocommerce_product_thumbnails_large_size', 'full' );
-			$post_thumbnail_id = get_post_thumbnail_id( $post->ID );
-			$full_size_image   = wp_get_attachment_image_src( $post_thumbnail_id, $thumbnail_size );
-			$placeholder       = has_post_thumbnail() ? 'with-images' : 'without-images';
-			$wrapper_classes   = apply_filters( 'woocommerce_single_product_image_gallery_classes', array(
-				'woocommerce-product-gallery',
-				'woocommerce-product-gallery--' . $placeholder,
-				'woocommerce-product-gallery--columns-' . absint( $columns ),
-				'images',
-			) );
-
-					
-			$html  = '<div data-thumb="' . get_the_post_thumbnail_url( $post->ID, 'woocommerce_thumbnail' ) . '" class="woocommerce-product-gallery__image"><a href="' . esc_url( $full_size_image[0] ) . '">';
-			$html .= get_the_post_thumbnail( $post->ID, 'woocommerce_single' );
-			$html .= '</a></div><!--oopsie-->';
-
-			return $html;
 	}
 
 }//fsi
